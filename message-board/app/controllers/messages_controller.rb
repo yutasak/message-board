2a5c7ba9724d0f9de1d261215b0419020d9fe409
yutasakurai <yuta-sakurai@united.jp>
2016-04-16 23:33:46 +0000

    Gemfile update for heroku

--- message-board/app/controllers/messages_controller.rb
+++ message-board/app/controllers/messages_controller.rb
@@ -1,20 +1,44 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
+
+
   def index
     @message = Message.new
-    ＃Messageを全て取得する。
-    @messages = Message.all 
-  end
-  
+    @messages=Message.all
+    end
+
   def create
     @message = Message.new(message_params)
-    @message.save
-    redirect_to root_path , notice:'メッセージを保存しました'
+    if @message.save
+      redirect_to root_path , notice: 'メッセージを保存しました'
+    else
+      # メッセージが保存できなかった時
+      @messages = Message.all
+      flash.now[:alert] = "メッセージの保存に失敗しました。"
+      render 'index'
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
   end
 
-private
-def message_params
- params.require(:message).permit(:name, :body)
- end
- ## ここまで
- end
- 
+   private
+    def message_params
+    params.require(:message).permit(:name, :body)
+    end
+  ## ここまで
+  end
+
+  def set_message
+    @message = Message.find(params[:id])
+  end
--- message-board/config/routes.rb
+++ message-board/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :messages 
-
-
+  root 'messages#index'
+ resources :messages , except: [:index, :new]
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
